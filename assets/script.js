cc9d6503389c2201b706be187653749a0885a98e
dewey preast <webwalkin@me.com>
2022-04-10 22:01:22 -0400

    major overhaul still reading an error

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
-import Quiz from "./quiz";
-import Question from "./question";
+// import Quiz from ".\assets\quiz.js";
+// import Question from ".\assets\question.js";
 
 const App = () => {
+  // this section is caching the following elements
   const quizEl = document.querySelector(".jquiz");
   const quizQuestionEl = document.querySelector(".jquiz__question");
   const trackerEl = document.querySelector(".jquiz__tracker");
@@ -29,12 +30,12 @@ const App = () => {
       "none of the above",
     ],
     0
-  );
+  )
   const q3 = new Question(
     "select all that apply 'What are all the types of pop up boxes available in Javascript",
     ["Alert", "confirm", "prompt", "alarm"],
     [0, 1, 2]
-  );
+  )
   const q4 = new Question(
     "What is the 'pop()' method in javascript",
     [
@@ -44,12 +45,12 @@ const App = () => {
       "The 'pop()' method is similar to the 'shift()' method, but the difference is that the 'shift()' method works at the array's start. The 'pop()' method takes the last element off  of the given array and returns it the array on which it is called is altered",
     ],
     3
-  );
+  )
   const q5 = new Question(
     "What are the two basic groups of data types in Javascript?",
     ["primitive", "ancient", "reference-types", "heritage"],
     [0, 2]
-  );
+  )
   const q6 = new Question(
     "What is the use of a 'typeof' Operator?",
     [
@@ -70,7 +71,38 @@ const App = () => {
     ],
     [0, 1, 2]
   )
-  const quiz = new quiz([q1,q2,q3,q4,q5,q6,q7]);
+  const quiz = new Quiz([q1,q2,q3,q4,q5,q6,q7]);
+
+  function Question(question, choices, answerKey){
+    this.question = question;
+    this.choices = choices;
+    this.answerKey = answerKey;
+}
+Question.prototype.isCorrect = function (guessKey){
+    return guessKey === this.answerKey;
+}
+function Quiz(questions){
+  this.questions = questions;
+  this.score = 0;
+  this.currentIndex = 0;
+}
+Quiz.prototype.getCurrentQuestion = function(){
+  return this.questions[this.currentIndex];
+}
+Quiz.prototype.hasEnded = function(){
+  this.currentIndex++;
+}
+Quiz.prototype.quess = function(userGuess) {
+  const currentQuestion = this.questions[this.currentIndex];
+  if (currentQuestion.isCorrect(userGuess)) {
+      this.score++;
+  }
+  this.nextIndex();
+}
+Quiz.prototype.reset = function(){
+  this.score = 0;
+  this.currentIndex = 0;
+}
 
   const listeners = _ => {
     nextButtonEl.addEventListener("click", function(){
@@ -102,7 +134,7 @@ const App = () => {
     choices;
     currentChoices.forEach((elem, index) => {
       markup +=
-      <li class = "jquiz__choice">
+      <li class = "jquiz__choice">                                               
         <input type="radio" name="choice"
         class= "jquiz__input" data-order= "${index}"
         id="choice${index}">
@@ -114,161 +146,51 @@ const App = () => {
       </li>
     });
     setValue(choicesEl, markup);
+  }     
+  const renderTracker = _ => { 
+    const index = quiz.currentIndex;
+    setValue(trackerEl, '${index+1} of ${quiz.questions.length}')
+  } 
+  const getPercentage = (num1,num2) => {
+    return Math.round((num1/num2) * 100);
   }
-  
-};
+  const launch = (width, maxPercent) => {
+    let loadingBar = setInterval(function(){
+      if (width > maxPercent) {
+        clearInterval(loadingBar);
+      } else {
+        width++;
+        progressInnerEl.getElementsByClassName.width = width = "%";
+      }
+    },3)
+  }
+  const renderProgress = _=> {
+    const currentWidth = getPercentage(quiz.currentIndex,quiz.questions.length);
+    launch(0, currentWidth);
+  }
+  const renderEndScreen = _ => {
+    setValue(quizQuestionEl,'great job!');
+    setValue(taglineEl, 'Complete!');
+    setValue(trackerEl, 'Your score : ${getPercentage(quiz.sore, quiz.question.length)}%');
+    nextButtonEl.getElementsByClassName.opacity = 0;
+    renderProgress();
+  }
+  const renderAll = _ => {
+    if (quiz.hasEnded()){
+      renderEndScreen();
+    } else {
+      renderQuestion();
+      renderChoicesElements();
+      renderTracker();
+      renderProgress();
+ }
+  }
+  return {
+    renderAll: renderAll, 
+    listeners: listeners
+  }
+  };
 
-// var startscreenEl = document.querySelector("#startscreen");
-// var startButtonEl = document.querySelector("#button");
-// var gameScreenEl = document.querySelector("#gameScreen");
-// var gameButtonEl = document.querySelector("#button");
-// var finishScreenEl = document.querySelector("#finish");
-// var answersEl = document.querySelector("#answers");
-// var questionsEl = document.querySelector("#questions");
-// var questionChoicesEl = document.querySelector("#questionChoices");
-// var correctEl = document.querySelector("#correct");
-// var correctTotalEl = document.querySelector("#correctTotal");
-// var initialsEl = document.querySelector("#initials");
-// var timeRemaining;
-// var timerEl = document.querySelector("#timer");
+  App.renderAll();
+  App.listeners();
 
-// var HIDE= "isHidden";
-
-// var correctTotal = [];
-
-// function userInitObj (initials, score){
-//   this.initials = initials;
-//   this.score = score;
-// }
-
-//      var questions =[
-//   {
-//     question: "What is called variable typing in Javascript",
-//     questionChoices:[
-// "Variable typing is used to assign a number to a variable, the same variable can be assigned to a string.",
-//    "adding variables to what you are currently typing",
-//    "arguments that change font after you type yes",
-//    "none of the above"],
-//        correct: 0
-//     },
-//   {
-
-//     question: "How can you convert the string of any base to an integer in javascript",
-//     questionChoices: [
-//       "by tying a knot around the number and pulling it in",
-//       "the 'parseInt()' function is used to convert numbers b/w different bases. 'parseInt()' takes the string to be converted as its first parameter",
-//       "by asking it nicely to change its attribute",
-//       "by changing the original string array value to null"],
-//       correct: 0
-//     },
-
-// {
-
-//     question : "select all that apply 'What is Undefined Value in Javascript'",
-//     questionChoices : [
-// "variable used in the code doesn't exist",
-// "variable is not assigned to any value",
-//  "property does not exist",
-//  "a value that cannot be read by human eyes"],
-//        correct: [0,1,2]
-//     },
-//     {
-//        question: "select all that apply 'What are all the types of pop up boxes available in Javascript",
-//        questionChoices :[
-// "Alert",
-// "confirm",
-// "prompt",
-// "alarm"//          correct: [0,1,2]
-// },
-
-//  {
-//   question: "What is the 'pop()' method in javascript",
-//   questionChoices : [
-//  "to add an element to the middle of the array",
-//   "to unlock an elements value",
-//   "to make a property of an array temporarily change attributes",
-//   "The 'pop()' method is similar to the 'shift()' method, but the difference is that the 'shift()' method works at the array's start. The 'pop()' method takes the last element off  of the given array and returns it the array on which it is called is altered"],
-//     correct: 3
-// },
-//  {
-// question : "What are the two basic groups of data types in Javascript?",
-// questionChoices : [
-// "primitive",
-// "ancient",
-// "reference-types",
-// "heritage"],
-//   correct: [0,2]
-// },
-
-// {
-//   question : "What is the use of a 'typeof' Operator?",
-//   questionChoices :[
-// " 'typeOf' is an operator used to return a string description of the type of a variable",
-// "to explain the font of an element",
-// " to generally describe the scope of a property",
-// "to loosely reference an array"],
-//     correct: 0
-
-//   }];
-// // establishing the position in the array of questions
-// var currentQuestion = 0;
-// // listing the elements that will change based on state of play
-// var dynamicElements=[
-//   startscreenel,
-//   gameScreenel,
-//   finishScreenEl
-//  ];
-// //  preparing event listeners and starting the 'correctTotal' tally
-// function init(){
-//   setEventlisteners();
-//   correctTotal();
-
-// }
-// // stores results of correct choices
-// function logResult() {
-//   localStorage.setItem("correctTotal",JSON.stringify(correctTotal));
-
-// }
-// // begins game
-// function initGame(){
-//   currentQuestion = 0;
-//   currentGame = new userInitObj("",0);
-//   populateQuestion(currentQuestion);
-//   updateTimer();
-// }
-// // Checks answer
-// function checkAnswer (currentQuestion, answerID){
-// console.log("answerID:" + answerID);
-// console.log("answer object:");
-// console.log(questions[currentQuestion]['answer']);
-// if (answerID == (questions[currentQuestion]['correct']) ){
-//   right(true);
-// } else {
-//   wrong(false);
-// }
-// function right(right){
-//   if (right){
-//     console.log("Before score:" + currentGame["score"])
-//     currentGame["score"]++
-//     console.log("After score:" + currentGame["score"])
-
-//   } else {
-//     currentGame["score"]--
-//     timeRemaining = timeRemaining - 5
-//   }
-// }
-// //grabs questions and answers as a list
-// function populateQuestion() {
-//   var questionObj = questions[currentQuestion];
-//    answersEl.innerHTML = "";
-//   questionsEl.textContent = questionObj.question;
-//   for (i = 0; i < questionObj.answers.length; i++) {
-//       var answer = questionObj.answers[i];
-//       var li = document.createElement("li");
-//       li.setAttribute("data-index",i);
-//       li.textContent = answer;
-//       answersEl.appendChild(li);
-//   };
-// }
-// }
-// init();
